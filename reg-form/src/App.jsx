4e96ca0d5c08b319c48643a64b81e761db215b93
Melnyk Zoriana <48826458+zoriana-melnyk@users.noreply.github.com>
2021-08-11 19:34:40 +0300

    work with reg-form

--- reg-form/src/App.jsx
+++ reg-form/src/App.jsx
@@ -9,22 +9,51 @@ class App extends React.Component {
     super(props);
     this.state = {
       isLoginActive: true,
+    };
+  }
+
+  componentDidMount() {
+    this.rightSide.classList.add("right");
+  }
+
+  changeState() {
+    const { isLoginActive } = this.state;
+    if (isLoginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
     }
+    else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("rigth");
+    }
+
+    this.setState(prevState => ({ isLoginActive: !prevState.isLoginActive }))
   }
 
   render() {
     const { isLoginActive } = this.state;
+    const current = isLoginActive ? "Register" : "Login";
+    const currentActive = isLoginActive ? "login" : "register";
     return (
       <div className="App">
         <div className="login">
           <div className="container">
-            {isLoginActive && <Login containerRef={(ref) => this.current = ref} />}
-            {!isLoginActive && <Register containerRef={(ref) => this.current = ref} />}
+            {isLoginActive && (<Login containerRef={(ref) => this.current = ref} />)}
+            {!isLoginActive && (<Register containerRef={(ref) => this.current = ref} />)}
           </div>
+          <RightSide current={current} currentActive={currentActive} containerRef={ref => this.rightSide = ref} onClick={this.changeState.bind(this)} />
         </div>
       </div>
     )
   }
+}
+
+const RightSide = props => {
+  return <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+    <div className="inner-container">
+      <div className="text">{props.current}</div>
+    </div>
+  </div>
 }
 
 export default App;
